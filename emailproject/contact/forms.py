e8f08f12746646f163057345ca5202a4b701e271
Gagan Deep Gautam <66736895+gagandeep141@users.noreply.github.com>
2022-12-10 15:52:47 +0000

    forms modified

--- emailproject/contact/forms.py
+++ emailproject/contact/forms.py
@@ -13,3 +13,21 @@ class ContactForm(forms.Form):
         cl_data=super().clean()
 
         name=cl_data.get('name').strip()
+        from_email=cl_data.get('email')
+        subject=cl_data.get('inquiry')
+
+        msg=f'{name} with email {from_email} said:'
+        msg+=f'\n"{subject}"\n\n'
+        msg+=cl_data.get('message')
+        return subject,msg
+    
+    def send(self):
+        subject,msg=self.get_info()
+
+        send_mail(
+            subject=subject,
+            message=msg,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.RECIPIENT_ADDRESS]
+        )
+
