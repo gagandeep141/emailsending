d104fdc1029baefa8e55b800ed7c9080619d88e0
Gagan Deep Gautam <66736895+gagandeep141@users.noreply.github.com>
2022-12-10 06:02:46 +0000

    forms update

--- emailproject/contact/forms.py
+++ emailproject/contact/forms.py
@@ -1,3 +1,15 @@
 from django import forms
 from django.conf import settings
 from django.core.mail import send_mail
+
+class ContactForm(forms.Form):
+    name=forms.CharField(max_length=120)
+    email=forms.EmailField()
+    inquiry=forms.CharField(max_length=80)
+    message=forms.CharField(widget=forms.Textarea)
+
+    def get_info(self):
+        # cleaned data
+        cl_data=super().clean()
+
+        name=cl_data.get('name').strip()
